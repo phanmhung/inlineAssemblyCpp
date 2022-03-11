8d05e526fa45c2958e99c2a8f40700a459f5fc29
Phan M. Hung <pmhungpro1@gmail.com>
2022-03-11 21:46:59 +0700

    Update asm1.cpp

--- lab1/asm1.cpp
+++ lab1/asm1.cpp
@@ -3,43 +3,36 @@ using namespace std;
 //Сложение
 int Add(int a, int b) {
     int c = 0;
-
     _asm {
         mov ebx, a
         add ebx, b
         mov c, ebx
     }
-
     return c;
 }
-
+//Вычитание
 int Sub(int a, int b) {
     int c = 0;
-
     _asm {
         mov ebx, a
         sub ebx, b
         mov c, ebx
     }
-
     return c;
 }
-
+//Умножение
 int Mul(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         imul b
         mov c, eax
     }
-
     return c;
 }
-
+//Деление
 int Div(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov edx, 0
@@ -47,13 +40,11 @@ int Div(int a, int b) {
         idiv ebx
         mov c, eax
     }
-
     return c;
 }
-
+//Операция ==
 int Equal(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -64,13 +55,11 @@ int Equal(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Операция >
 int GreaterThan(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -81,13 +70,11 @@ int GreaterThan(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Операция <
 int LessThan(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -98,13 +85,11 @@ int LessThan(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Операция !=
 int NotEqual(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -115,13 +100,11 @@ int NotEqual(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Операция <=
 int LessOrEqual(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -132,13 +115,11 @@ int LessOrEqual(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Операция >=
 int GreaterOrEqual(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
@@ -149,71 +130,60 @@ int GreaterOrEqual(int a, int b) {
         yes:
         mov c, ecx
     }
-
     return c;
 }
-
+//Логические операции
 int Not(int a) {
     int c = 0;
-
     _asm {
         mov eax, a
         not eax
         mov c, eax
     }
-
     return c;
 }
 
 int And(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
         and eax, ebx
         mov c, eax
     }
-
     return c;
 }
 
 int Or(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
         or eax, ebx
         mov c, eax
     }
-
     return c;
 }
 
 int Xor(int a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
         xor eax, ebx
         mov c, eax
     }
-
     return c;
 }
-
+//Индексация по массиву
 int ElementByIndex(int* a, int b) {
     int c = 0;
-
     _asm {
         mov eax, a
         mov ebx, b
         mov ebx, [eax + 4 * ebx]
         mov c, ebx
     }
-
     return c;
 }
 
@@ -271,17 +241,18 @@ int CicleLeft(unsigned char a, unsigned char b) {
 
 int main()
 {
-    cout << "21 + 21 = " << Add(21, 21) << endl; 
-    cout << "0 - 42 = " << Sub(0, 42) << endl;
-    cout << "-21 * 2 = " << Mul(-21, 2) << endl;
-    cout << "210 / 5 = " << Div(210, 5) << endl;
+    cout << "20 + 21 = " << Add(20, 21) << endl; 
+    cout << "0 - 32 = " << Sub(0, 32) << endl;
+    cout << "-13 * 2 = " << Mul(-13, 2) << endl;
+    cout << "200 / 5 = " << Div(200, 5) << endl;
 
     cout << endl;
 
-    cout << "5 == 5 ? " << Equal(5, 5) << endl;
-    cout << "5 > 3 ? " << GreaterThan(5, 3) << endl;
-    cout << "5 < 3 ? " << LessThan(5, 3) << endl;
-    cout << "5 <= 5 ? " << LessOrEqual(5, 5) << endl;
+    cout << "1 == 1 ? " << Equal(1, 1) << endl;
+    cout << "10 == 11 ? " << Equal(10, 11) << endl;
+    cout << "7 > 3 ? " << GreaterThan(7, 3) << endl;
+    cout << "91 < 43 ? " << LessThan(91, 43) << endl;
+    cout << "15 <= 25 ? " << LessOrEqual(15, 25) << endl;
     cout << "5 >= 2 ? " << GreaterOrEqual(5, 2) << endl;
     cout << "5 != 5 ? " << NotEqual(5, 5) << endl;
     
